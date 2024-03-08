9b98c032a0b04ae38dc3ad04f583c5c0b84a78ad
cjackson100 <158119520+cjackson100@users.noreply.github.com>
2024-03-07 21:52:44 -0500

    Ice 2

---
+++ ICE2_CJackson/calculator.ts
@@ -0,0 +1,20 @@
+// ICE 2: Javascript to Typescript calculator.ts file
+// Edited by Christina Jackson
+// March 7 2024
+// Javascript to Typescript conversion in class assignment for INFT-2202-07
+
+export let add = (a: number, b: number) => {
+  return a + b;
+}
+
+export let subtract = (a: number, b:number) => {
+  return a - b;
+}
+
+export let multiply = (a: number, b: number) => {
+  return a * b;
+}
+
+export let divide = (a: number, b: number) => {
+  return a / b;
+}
---
+++ ICE2_CJackson/index.ts
@@ -0,0 +1,71 @@
+// ICE 2: Javascript to Typescript index.ts file
+// Edited by Christina Jackson
+// March 7 2024
+// Javascript to Typescript conversion in class assignment for INFT-2202-07
+
+
+import {
+  add,
+  subtract,
+  multiply,
+  divide,
+}
+from "./calculator";
+
+import * as readline from "readline-sync";
+
+
+
+var readInput = () => {
+  console.log("Welcome to the calculator. Choose one of the following options");
+  console.log("1. add");
+  console.log("2. subtract");
+  console.log("3. multiply");
+  console.log("4. divide");
+  console.log("5. exit");
+
+  let option = readline.question(">> ");
+
+  if (option !== "5") {
+    console.log("Enter the first number");
+    let a = Number(readline.question(">> "));
+
+    console.log("Enter the second number");
+    let b = Number(readline.question(">> "));
+
+    let c: number;
+    switch(option){
+      case "1": {
+        c = add(a, b);
+        console.log(`a + b = ${c}`);
+      }
+      break;
+
+      case "2": {
+        c = subtract(a, b);
+        console.log(`a - b = ${c}`);
+      }
+      break;
+
+      case "3": {
+        c = multiply(a, b);
+        console.log(`a * b = ${c}`);
+      }
+      break;
+
+      case "4": {
+        c = divide(a, b);
+        console.log(`a / b = ${c}`);
+      }
+      break;
+    }    
+
+    readInput();
+  }
+}
+
+readInput();
+
+console.log("Thank you for using calculator. Good Bye");
+
+
